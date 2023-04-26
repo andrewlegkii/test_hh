17278064717aeae0de41b43f069f7f57af15eaf3
andrewlegkii <legkiy.andrey@bk.ru>
2023-04-26 22:39:20 +0300

    init2

--- eth.py
+++ eth.py
@@ -2,11 +2,10 @@ import websocket
 import json
 import time
 import telegram
-from telegram import Update
+
 from telegram.ext import (Updater,
                           CommandHandler,
-                          CallbackQueryHandler,
-                          CallbackContext)
+                          CallbackQueryHandler)
 
 bot_token = '6155355153:AAFdVi1QCj_3g4Z3Rll-0W74cZwBwnC5KH8'
 chat_id = '360300829'
@@ -30,32 +29,36 @@ def on_message(ws, message):
         last_price = price
 
 def on_close(ws):
-    print("Ошибка подключения.")
+    print("Connection closed.")
 
-def start(update: Update, context: CallbackContext):
+def start(update, context):
     bot = context.bot
-    text = "Нажмите на кнопку для получения цены ETHUSDT."
+    text = "Press the button to get the price of ETHUSDT."
     button = telegram.InlineKeyboardButton(text="Get ETHUSDT Price", callback_data="get_price")
     keyboard = telegram.InlineKeyboardMarkup([[button]])
     bot.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
 
-def button(update: Update, context: CallbackContext):
+def button(update, context):
     query = update.callback_query
     if query.data == "get_price":
         if last_price is not None:
             text = f"Current price of ETHUSDT futures: {last_price:.2f}"
         else:
-            text = "Попробуйте позже."
+            text = "Please try again later."
         query.answer(text=text)
 
-updater = Updater(bot_token)
+def main():
+    updater = Updater(bot_token, use_context=True)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
-updater.start_polling()
+    updater.start_polling()
 
-ws = websocket.WebSocketApp('wss://stream.binance.com:9443/ws/ethusdt@kline_1m',
-                            on_message=on_message,
-                            on_close=on_close)
-ws.run_forever()
+    ws = websocket.WebSocketApp('wss://stream.binance.com:9443/ws/ethusdt@kline_1m',
+                                on_message=on_message,
+                                on_close=on_close)
+    ws.run_forever()
+
+if __name__ == '__main__':
+    main()
